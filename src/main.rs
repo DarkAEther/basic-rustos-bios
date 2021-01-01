5b6ea1c76e93a51f9b663e28cd47ab35fb9d80d8
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2021-01-02 01:39:28 +0530

    Enable testing for the OS

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
 #![feature(core_intrinsics)]
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::intrinsics;
+use core::panic::PanicInfo;
 
 extern crate bootloader;
 
 mod vga_buffer;
 
-use core::intrinsics;
-use core::panic::PanicInfo;
-
 #[panic_handler]
 #[no_mangle]
 fn panic(_info: &PanicInfo) -> !{
-    intrinsics::abort()
+    println!("{}",_info);
+    loop{}
+    //intrinsics::abort()
 }
 
 #[no_mangle]
 pub fn _start() -> ! {
-    use core::fmt::Write; // import the trait to use the macro
+    //use core::fmt::Write; // import the trait to use the macro
     // with spinlock mutex, the lock() method directly returns the reference instead of Result<T, E> since this is a spinlock and it blocks until the lock is available.
-    write!(vga_buffer::WRITER.lock(),"Hello, {}, {}", 42, 1.337).unwrap();
+    //write!(vga_buffer::WRITER.lock(),"Hello, {}, {}", 42, 1.337).unwrap();
+    
+    println!("Hello world!");
+
+    #[cfg(test)]
+    test_main();
+
     loop{}
+    
+}
+
+#[cfg(test)]
+//only include in a test config
+pub fn test_runner(tests: &[&dyn Fn()]){
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion(){
+    print!("trivial assertion... ");
+    assert_eq!(1,1);
+    println!("ok");
 }
